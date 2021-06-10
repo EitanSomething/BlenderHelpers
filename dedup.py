4ae40461764ca77d300a232070c43b63aaab37cc
EitanSomething <37350423+EitanSomething@users.noreply.github.com>
2021-06-09 20:32:41 -0400

    Update dedup.py

--- dedup.py
+++ dedup.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
 def do():
+  f = open("logs.json", "a")
+  f.truncate()
+  f.close()
   clean_data = OrderedDict()
   with open("logs.json", "r") as f:
     [clean_data.setdefault(line.strip()) for line in f if line.strip() != ""]
