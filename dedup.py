a94269e180cdd259077db468ce03f16d7aa481e1
EitanSomething <37350423+EitanSomething@users.noreply.github.com>
2021-06-10 09:12:43 -0400

    Update dedup.py

--- dedup.py
+++ dedup.py
@@ -2,10 +2,10 @@ from collections import OrderedDict
 
 def do():
   clean_data = OrderedDict()
-  with open("logs.json", "r") as f:
+  with open("logs.md", "r") as f:
     [clean_data.setdefault(line.strip()) for line in f if line.strip() != ""]
   
-  with open("logs.json", "w") as f:
+  with open("logs.md", "w") as f:
     [print(one, file=f) for one in clean_data]
 
 if __name__ == '__main__':
