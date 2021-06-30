a9542de0e17f00698efd4b3b737ab321850e4320
EitanSomething <eitant13@gmail.com>
2021-06-30 14:28:27 -0400

    Clean up code

--- scan_stories.py
+++ scan_stories.py
@@ -1,13 +1,36 @@
-import sched_user
-import sched_feed
-from sched_feed import stories
-from sched_user import user_queue
+import phabricator
+import time
+import typing
+import json
 
 
-def scan_stories():
+LIMIT = 100
+phab = None
+stories = dict()
+user_queue: typing.List[str] = []
 
+
+def setup_phab():
+    global phab
+    phab = phabricator.Phabricator(timeout=10)
+    phab.update_interfaces()
+
+
+def fetch_new_items():
+    global phab, stories
+    # num_stories = LIMIT
+    new_stories = phabricator.Result(phab.feed.query(limit=LIMIT, view="data"))
+    if len(new_stories.items()) > 0:
+        stories.update(new_stories)
+
+
+def filter_stories():
+    global stories, user_queue
     for story in stories.values():
         if "transactionPHIDs" not in story["data"]:
+            continue
+        if not story["data"]["isDisabled"]:
+            print("Disabled")
             continue
         if story["authorPHID"] == story["data"]["objectPHID"]:
             transactionPHID = list(story["data"]["transactionPHIDs"].keys())[0]
@@ -15,10 +38,28 @@ def scan_stories():
                 user_queue.append(story["authorPHID"])
 
 
-while True:
-    sched_feed.fetch_new_items()
-    scan_stories()
-    sched_user.user_url()
+def get_user_urls():
+    global user_queue, phab
+    while len(user_queue) > 0:
+        ln = min(len(user_queue), LIMIT)
+        user_data = phab.user.search(
+            constraints={"phids": user_queue[:ln]}, limit=LIMIT)
+        user_queue = user_queue[ln:]
+        f = open("logs.md", "r")
+        safe = open("safe.md", "r")
+
+        for phab_data in user_data["data"]:
+            data = "https://developer.blender.org/p/" + phab_data["fields"]["username"]
+            if data not in f.read():
+                if data not in safe.read():
+                    print(json.dumps(data))
+        time.sleep(6)
+
+
+if __name__ == '__main__':
+    setup_phab()
+    fetch_new_items()
+    filter_stories()
+    # get_user_urls()
     # Make sure there's always something to commit.
     print(" ")
-    break
