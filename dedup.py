04057b8db4b8dcfa0de0f4a6493425c2f032d031
z-aki <39851968+z-aki@users.noreply.github.com>
2021-08-24 14:49:54 +0530

    Left out in e36decbcb22035d05ac3b76738ad1a9d102b2027

--- dedup.py
+++ dedup.py
@@ -6,7 +6,7 @@ def do():
     [clean_data.setdefault(line.strip()) for line in f if line.strip() != ""]
   
   with open("logs.md", "w") as f:
-    [print(one, file=f) for one in clean_data]
+    [print(one, file=f, end="\n\n") for one in clean_data]
 
 if __name__ == '__main__':
   do()
