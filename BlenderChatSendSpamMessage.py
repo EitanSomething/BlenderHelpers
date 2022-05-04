f98fbacc1cced2278124864db5f5648d1248d898
Eitan Traurig <37350423+EitanSomething@users.noreply.github.com>
2022-05-04 22:28:29 +0300

    change to BlenderHelpers

--- BlenderChatSendSpamMessage.py
+++ BlenderChatSendSpamMessage.py
@@ -9,7 +9,7 @@ def login():
     api = RocketChatAPI(settings={'token': TOKEN, 'user_id': USERID,
                                   'domain': 'https://blender.chat'})
 def sendMessage():
-    api.send_message("@ThomasDinges Potential Spam. Not everything on the list is spam. https://github.com/EitanSomething/BlenderSpam/blob/auto_commits/logs.md","LpczsRCWtkYY6AzFt")
+    api.send_message("@ThomasDinges Potential Spam. Not everything on the list is spam. https://github.com/EitanSomething/BlenderBlenderHelpers/blob/auto_commits/logs.md","LpczsRCWtkYY6AzFt")
 if __name__ == '__main__':
     login()
     sendMessage()
